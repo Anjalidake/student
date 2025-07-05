f32736988541e1b306a7b5a634df666be440ecd3
Anjalidake <anjalidake98@gmail.com>
2025-07-05 23:24:57 +0530

    Update StudentRepo.java

--- springboot/src/main/java/com/example/Repository/StudentRepo.java
+++ springboot/src/main/java/com/example/Repository/StudentRepo.java
@@ -3,7 +3,7 @@ package com.example.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.pojo.StudentPojo;
-
+// ---------------------------
 public interface StudentRepo extends JpaRepository<StudentPojo, Integer> {
 
 }
