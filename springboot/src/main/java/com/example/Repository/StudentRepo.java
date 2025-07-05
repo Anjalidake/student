449fcd2fb388abbfee544291accc3b6aa83d695e
Anjali <anjalidake98@gmail.com>
2025-07-05 23:28:30 +0530

    for merge purpose

--- springboot/src/main/java/com/example/Repository/StudentRepo.java
+++ springboot/src/main/java/com/example/Repository/StudentRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.pojo.StudentPojo;
 // ---------------------------
+////////////////// this is for merge request/ PR
 public interface StudentRepo extends JpaRepository<StudentPojo, Integer> {
 
 }
